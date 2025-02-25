518e2ba56098da764d06d15d68a12a4d71d04f3f
Suhas Cristy Mathey <132161396+Cristy210@users.noreply.github.com>
2025-02-24 21:58:12 -0500

    Subspace Clustering Package Code

--- src/SubspaceClustering.jl
+++ src/SubspaceClustering.jl
@@ -2,4 +2,72 @@ module SubspaceClustering
 
 # Write your package code here.
 
+## K-Subspaces SubspaceClustering
+
+#Imports
+import LinearAlgebra: norm, svd, transpose
+import Base: copy, deepcopy, findall, view
+import Base:argmax
+using Random: randn
+using Logging: @info
+using ProgressLogging: @progress
+using ArnoldiMethod: partialschur
+
+#Polar function
+
+function polar(X)
+	U, _, V = svd(X)
+	U*V'
+end
+
+"""
+	KSS(X, d; niters=100)
+
+Run K-subspaces on the data matrix `X`
+with subspace dimensions `d[1], ..., d[K]`,
+treating the columns of `X` as the datapoints.
+"""
+function KSS(X, d; niters=100, Uinit=polar.(randn.(size(X, 1), collect(d))))
+	K = length(d)
+	D, N = size(X)
+
+	# Initialize
+	U = deepcopy(Uinit)
+	c = [argmax(norm(U[k]' * view(X, :, i)) for k in 1:K) for i in 1:N]
+	c_prev = copy(c)
+
+	# Iterations
+	@progress for t in 1:niters
+		# Update subspaces
+		for k in 1:K
+			ilist = findall(==(k), c)
+			# println("Cluster $k, ilist size: ", length(ilist))
+			if isempty(ilist)
+				# println("Initializing $k subspace")
+				U[k] = polar(randn(D, d[k]))
+			else
+				A = view(X, :, ilist) * transpose(view(X, :, ilist))
+				decomp, history = partialschur(A; nev=d[k], which=:LR)
+				@show history
+				# U[k] = tsvd(view(X, :, ilist), d[k])[1]
+				U[k] = decomp.Q
+			end
+		end
+
+		# Update clusters
+		for i in 1:N
+			c[i] = argmax(norm(U[k]' * view(X, :, i)) for k in 1:K)
+		end
+
+		# Break if clusters did not change, update otherwise
+		if c == c_prev
+			@info "Terminated early at iteration $t"
+			break
+		end
+		c_prev .= c
+	end
+
+	return U, c
+end
+
 end
