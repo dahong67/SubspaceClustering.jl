f5cf2bde9248f63223e01ee636e0d2fa9c122d4e
David Hong <hong@udel.edu>
2025-04-25 11:29:52 -0400

    Refactor tests
    Also removed unecessary imports and did some cleaning up.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,3 @@
 using TestItemRunner
 
-@testitem "Code quality (Aqua.jl)" begin
-    using Aqua
-    using SubspaceClustering
-    Aqua.test_all(SubspaceClustering)
-end
-
-@testitem "Code linting (JET.jl)" begin
-    using JET
-    using SubspaceClustering
-    JET.test_package(SubspaceClustering; target_defined_modules = true)
-end
-
 @run_package_tests
