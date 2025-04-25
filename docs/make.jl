aaf7187766b471b543f1906477088911878ea218
David Hong <hong@udel.edu>
2025-04-25 14:30:04 -0400

    Revert changes to docs
    Plan to move this work to a new PR

--- docs/make.jl
+++ docs/make.jl
@@ -1,27 +1,23 @@
 using SubspaceClustering
 using Documenter
 
-DocMeta.setdocmeta!(
-    SubspaceClustering,
-    :DocTestSetup,
-    :(using SubspaceClustering);
-    recursive = true,
-)
+DocMeta.setdocmeta!(SubspaceClustering, :DocTestSetup, :(using SubspaceClustering); recursive=true)
 
 makedocs(;
-    modules = [SubspaceClustering],
-    authors = "David Hong <hong@udel.edu> and contributors",
-    sitename = "SubspaceClustering.jl",
-    format = Documenter.HTML(;
-        canonical = "https://dahong67.github.io/SubspaceClustering.jl",
-        edit_link = "main",
-        assets = String[],
+    modules=[SubspaceClustering],
+    authors="David Hong <hong@udel.edu> and contributors",
+    sitename="SubspaceClustering.jl",
+    format=Documenter.HTML(;
+        canonical="https://dahong67.github.io/SubspaceClustering.jl",
+        edit_link="main",
+        assets=String[],
     ),
-    pages = [
+    pages=[
         "Home" => "index.md",
-        "Qucik start guide" => "quickstart.md",
-        "Manual" => ["Overview" => "man/basics.md", "Inputs" => "man/inputs.md"],
     ],
 )
 
-deploydocs(; repo = "github.com/dahong67/SubspaceClustering.jl", devbranch = "main")
+deploydocs(;
+    repo="github.com/dahong67/SubspaceClustering.jl",
+    devbranch="main",
+)
--- docs/src/man/inputs.md
+++
@@ -1,5 +0,0 @@
-# Inputs
-
-Clustering algorithm accepts input data matrix in the following form:
-
-    - Data matrix ``X`` of size `` D \times N ``, where `X[:, i]` is an ``i``the data point in a ``D``- dimensional feature space. 
