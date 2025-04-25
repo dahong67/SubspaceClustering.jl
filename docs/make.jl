fe3da8c3e19a6c2efbf241190b181343dc5af805
David Hong <hong@udel.edu>
2025-04-25 14:41:09 -0400

    Run formatter

--- docs/make.jl
+++ docs/make.jl
@@ -1,23 +1,23 @@
 using SubspaceClustering
 using Documenter
 
-DocMeta.setdocmeta!(SubspaceClustering, :DocTestSetup, :(using SubspaceClustering); recursive=true)
+DocMeta.setdocmeta!(
+    SubspaceClustering,
+    :DocTestSetup,
+    :(using SubspaceClustering);
+    recursive = true,
+)
 
 makedocs(;
-    modules=[SubspaceClustering],
-    authors="David Hong <hong@udel.edu> and contributors",
-    sitename="SubspaceClustering.jl",
-    format=Documenter.HTML(;
-        canonical="https://dahong67.github.io/SubspaceClustering.jl",
-        edit_link="main",
-        assets=String[],
+    modules = [SubspaceClustering],
+    authors = "David Hong <hong@udel.edu> and contributors",
+    sitename = "SubspaceClustering.jl",
+    format = Documenter.HTML(;
+        canonical = "https://dahong67.github.io/SubspaceClustering.jl",
+        edit_link = "main",
+        assets = String[],
     ),
-    pages=[
-        "Home" => "index.md",
-    ],
+    pages = ["Home" => "index.md"],
 )
 
-deploydocs(;
-    repo="github.com/dahong67/SubspaceClustering.jl",
-    devbranch="main",
-)
+deploydocs(; repo = "github.com/dahong67/SubspaceClustering.jl", devbranch = "main")
