9975bede19e528894919f1507e69ba73ddeacd05
David Hong <dahong67@users.noreply.github.com>
2025-04-26 00:38:10 -0400

    Improve docs setup (#5)
    * Use new `sources` field for docs Manifest
    
    Standard approach in the past was to always dev the package
    before building its docs (as done in the github workflow).
    Alternatively, one could commit the `Manifest.toml` file.
    
    This commit switches to using the new `sources` field in `Project.toml`
    added in Julia 1.11: https://docs.julialang.org/en/v1/NEWS/#Package-Manager
    Inspired by discussions in: https://github.com/JuliaDocs/Documenter.jl/issues/1413
    And example in: https://github.com/JuliaDocs/Documenter.jl/pull/2653
    
    * Add script for serving live version locally
    
    Based on: https://github.com/dahong67/GCPDecompositions.jl/blob/4900abee06c2e16a0bc70958f10ed7715a6707ab/docs/serve.jl
    
    * Set Base.exit_on_sigint(false) to gracefully exit
    
    This makes sure the interrupt exception is correctly handled.
    See:
    https://docs.julialang.org/en/v1/base/base/#Core.InterruptException
    https://docs.julialang.org/en/v1/base/c/#Base.exit_on_sigint
    
    * Attempt to get working with Revise
    
    * Revert "Attempt to get working with Revise"
    
    This reverts commit 9af05f2b33c45cbc084031e3b0bdd75d062731c8.
    
    * Document documentation workflow

---
+++ docs/serve.jl
@@ -0,0 +1,15 @@
+## Serve a live version of the documentation
+
+# Check that Julia version is at least v1.11
+# (needed for the `sources` field used in `docs/Project.toml`)
+VERSION >= v"1.11" || error("Julia version must be at least v1.11")
+
+# Activate and instantiate the `docs` environment
+using Pkg
+Pkg.activate(@__DIR__)
+Pkg.instantiate()
+
+# Load packages and serve the docs
+using SubspaceClustering, LiveServer
+Base.exit_on_sigint(false)
+cd(servedocs, pkgdir(SubspaceClustering))
