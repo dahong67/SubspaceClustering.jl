3f86b69b4b49c98267e40acbaa7dc6a11c6f1ec2
Suhas Cristy Mathey <132161396+Cristy210@users.noreply.github.com>
2026-03-05 20:56:25 -0500

    TSC Algorithm - Auto Selection for `K` Clusters

--- test/items/algorithms/tsc.jl
+++ test/items/algorithms/tsc.jl
@@ -6,27 +6,27 @@
     @testset "invalid number of clusters" begin
         rng = StableRNG(4)
         X = randn(rng, 5, 20)
-        @test_throws ArgumentError tsc(X, 0)
-        @test_throws ArgumentError tsc(X, -1)
-        @test_throws ArgumentError tsc(X, size(X, 2) + 1)
+        @test_throws ArgumentError tsc(X; K = 0)
+        @test_throws ArgumentError tsc(X; K = -1)
+        @test_throws ArgumentError tsc(X; K = size(X, 2) + 1)
     end
 
     @testset "invalid maximum number of neighbors" begin
         rng = StableRNG(4)
         X = randn(rng, 5, 20)
-        @test_throws ArgumentError tsc(X, 2; max_nz = 0)
+        @test_throws ArgumentError tsc(X; K=2, max_nz = 0)
     end
 
     @testset "invalid maximum chunk size" begin
         rng = StableRNG(4)
         X = randn(rng, 5, 20)
-        @test_throws ArgumentError tsc(X, 2; max_chunksize = 0)
+        @test_throws ArgumentError tsc(X; K=2, max_chunksize = 0)
     end
 
     @testset "invalid number of K-means runs" begin
         rng = StableRNG(4)
         X = randn(rng, 5, 20)
-        @test_throws ArgumentError tsc(X, 2; kmeans_nruns = 0)
+        @test_throws ArgumentError tsc(X; K=2, kmeans_nruns = 0)
     end
 end
 
@@ -35,7 +35,7 @@ end
 
     rng = StableRNG(4)
     X = reduce(hcat, [svd(randn(rng, 100, 2)).U * randn(rng, 2, 4) for _ in 1:3])
-    result = tsc(X, 3; rng)
+    result = tsc(X; K=3, rng)
 
     @test Set([findall(==(k), result.assignments) for k in 1:3]) == Set([1:4, 5:8, 9:12])
 end
