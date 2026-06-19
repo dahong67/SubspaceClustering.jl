389cadea3bbeca73af73d36c2eae3a1b76efe9d3
Suhas Cristy Mathey <132161396+Cristy210@users.noreply.github.com>
2026-06-19 15:42:48 -0400

    docstring for `batch` function; Minor Tweaks

--- src/meta/batch.jl
+++ src/meta/batch.jl
@@ -1,17 +1,34 @@
 ## Batch KSS and KAS Algorithms
 
 """
-    batch(alg, X, d)
+    batch(alg::Union{typeof(kss), typeof(kas)}, X::AbstractMatrix{<:Number},d::AbstractVector{<:Integer};
+    nruns::Integer = 50,
+    maxiters::Integer = 100,
+    showprogress::Bool = false)
+
+Both K-Subpaces (KSS) and K-Affinespaces (KAS) may get stuck in poor local minima depending on the initialization. Run `kss` and `kas` algorithm multiple times using different random initializations and return the run with the lowest cost.
+
+# Arguments
+- `alg::Union{typeof(kss), typeof(kas)}`: Clustering algorithm to run
+- `X::AbstractMatrix{<:Number}`: Data matrix whose columns are observations
+- `d::AbstractVector{<:Integer}`: Dimensions of the subspaces/affine spaces
+
+# Keyword arguments
+- `nruns::Integer = 50`: Number of independent runs
+- `maxiters::Integer = 100`: Maximum number of iterations per run
+- `showprogress::Bool = false`: whether to log progress for different algorithm runs
+
+See also [`kss`](@ref), [`kas`](@ref).
 """
 function batch(
-    alg::Function,
+    alg::Union{typeof(kss), typeof(kas)},
     X::AbstractMatrix{<:Number},
     d::AbstractVector{<:Integer};
     nruns::Integer = 50,
     maxiters::Integer = 100,
     showprogress::Bool = false,
 )
-    # check nruns
+    # check number of runs
     nruns > 0 || throw(ArgumentError("nruns must be positive. Got `nruns=$nruns`"))
 
     runs = @withprogressif showprogress map(1:nruns) do idx
@@ -25,5 +42,5 @@ function batch(
     nconverged = count(run -> run.converged, runs)
     @info "$nconverged/$nruns runs converged"
 
-    return first(sort(runs; by = run -> run.totalcost))
+    return runs[argmin([run.totalcost for run in runs])]
 end
